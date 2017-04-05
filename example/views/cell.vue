13a8047b924a962cee3da87dfa7fd6f3d6c9fc19
bigezhang <894239925@qq.com>
2017-04-05 11:35:16 +0800

    cell demo

--- example/views/cell.vue
+++ example/views/cell.vue
@@ -1,6 +1,16 @@
 <template lang="html">
-  <div class="">
-    cell
+  <div class="page-cell">
+    <bz-cell title="标题文字"></bz-cell>
+    <bz-cell title="标题文字" value="说明文字"></bz-cell>
+    <bz-cell title="标题文字" icon="more" value="带 icon"></bz-cell>
+    <bz-cell title="标题文字" icon="more">
+      <span>icon 图片</span>
+      <img src="https://placeholdit.imgix.net/~text?txtsize=9&txt=100%C3%97100&w=100&h=100" width="24" height="24" slot="icon" alt="" />
+    </bz-cell>
+    <bz-cell title="标题文字" is-link value="链接"></bz-cell>
+    <bz-cell title="标题文字">
+      <bz-button size="small" type="primary" icon="back">按钮</bz-button>
+    </bz-cell>
   </div>
 </template>
 
@@ -16,5 +26,5 @@ export default {
 }
 </script>
 
-<style lang="css">
+<style lang="scss">
 </style>
