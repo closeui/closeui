f91d0368684891e9087ddb6c861d5c63f2bdb583
bigezhang <894239925@qq.com>
2017-10-16 16:34:16 +0800

    demo list

--- example/components/demonav.vue
+++ example/components/demonav.vue
@@ -1,7 +1,18 @@
 <template>
   <div class="demo-nav-group">
-    <div class="demo-nav-group__title demo-nav-group__basetitle" :class="{'demo-nav-group__title--open': isOpen}">
+    <div class="demo-nav-group__title demo-nav-group__basetitle" :class="{'demo-nav-group__title--open': isOpen}" @click="handleNavTitle">
       {{group.groupName}}
+    </div>
+    <div class="demo-nav-group__list-wrapper" :class="{'demo-nav-group__list-wrapper--open': isOpen}">
+      <ul class="demo-nav-group__list" :class="{'demo-nav-group__list--open': isOpen}">
+        <template v-for="(navItem, index) in group.list">
+          <li :key="index" class="demo-nav-group__title" v-if="!navItem.disabled">
+            <router-link active-class="active" :to="base + navItem.path">
+              <p>{{navItem.title}}</p>
+            </router-link>
+          </li>
+        </template>
+      </ul>
     </div>
   </div>
 </template>
@@ -25,9 +36,16 @@ export default {
   },
   beforeCreate () {},
   created () {},
-  mounted () {},
+  mounted () {
+    this.isOpen = JSON.parse(sessionStorage.getItem('demo-nav-' + this.navKey))
+  },
   computed: {},
-  methods: {},
+  methods: {
+    handleNavTitle () {
+      this.isOpen = !this.isOpen
+      sessionStorage.setItem('demo-nav-' + this.navKey, this.isOpen)
+    }
+  },
   components: {}
 }
 </script>
@@ -46,6 +64,50 @@ export default {
     line-height: 56px;
     position: relative;
     user-select: none;
+    &.demo-nav-group__title--open {
+      color: #999;
+    }
+    a {
+      color: #333;
+      display: block;
+      user-select: none;
+      padding-left: 20px;
+      -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
+      &:active {
+        background: #ececec;
+      }
+      
+      > p {
+        border-top: 1px solid #e5e5e5;
+        -webkit-margin-before: 0em;
+        -webkit-margin-after: 0em;
+      }
+    }
+  }
+  
+  .demo-nav-group__list-wrapper {
+    height: 0;
+    overflow: hidden;
+    &.demo-nav-group__list-wrapper--open {
+      height: auto;
+    }
+  }
+  
+  .demo-nav-group__list {
+    transform: translateY(-50%);
+    transition: transform .2s ease-out;
+    &.demo-nav-group__list--open {
+      transform: translateY(0);
+    }
+  }
+  
+  li {
+    list-style: none;
+  }
+  ul {
+    padding: 0;
+    margin: 0;
+    overflow: hidden;
   }
 }
 </style>
